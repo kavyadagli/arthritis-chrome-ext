9b5e5b8550fdfda7d5850de5aafc4a230ac54e1d
kavyadagli <kdagli@terpmail.umd.edu>
2018-11-10 18:25:37 -0500

    updated js

--- popup.js
+++ popup.js
@@ -13,8 +13,22 @@
 //  });
 // };
 
+//store question answers
+var info = new Object();
+var info = {};
 
-let form = document.getElementsById('question');
+//get answer for question 1
+let form = document.getElementById('question1');
 form.addEventListener('click', function() {
-  console.log(form.value);
+  yes = document.getElementById('yes').value;
+  medium = document.getElementById('medium').value;
+  no = document.getElementById('no').value;
+
+  if (yes.clicked) {
+    dict[1] = "yes";
+  } else if (medium.clicked) {
+    dict[1] = "a little";
+  } else if (no.clicked) {
+    dict[1] = "no";
+  }
 });
