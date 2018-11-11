28c3b3ff87e9735d53287f2a0f12a6f420224bce
kavyadagli <kdagli@terpmail.umd.edu>
2018-11-10 23:06:16 -0500

    updated javascript

--- popup.js
+++ popup.js
@@ -17,41 +17,40 @@
 var info = new Object();
 var info = {};
 
-//get answer for question 1
-let form1 = document.getElementById('form1');
-form.addEventListener('click', function() {
-  yes = document.getElementById('yes').value;
-  no = document.getElementById('no').value;
 
-  if (yes.clicked) {
-    dict[1] = 1;
-  } else if (no.clicked) {
-    dict[1] = 0;
-  }
-});
+var questions = [
+  "Did you experience morning stiffness?",
+  "Did you take your medicine today?",
+  "Were you able to write well today?",
+  "Were you really fatigued?",
+  "Was walking long distances difficult for you?"
+]
 
-var elements = ['question1', 'question2', 'question3', 'question4', 'question5'];
 var idx = 0;
-
-for (var i = 1; i < elements.length; i++) {
-  document.getElementById(elements[i]).style.display = 'none';
-}
 
 let button = document.getElementById('next');
 button.addEventListener('click', function() {
 
-  document.getElementById(elements[idx]).style.display = 'block';
 
 
-  if (idx == 0) {
-    document.getElementById(elements[elements.length-1]).style.display = 'none';
-    idx++;
-  } else if (idx > 0 && idx < elements.length-1) {
-    document.getElementById(elements[idx-1]).style.display = 'none';
-    idx++;
-  } else {
-    document.getElementById(elements[idx-1]).style.display = 'none';
+  var selected = document.forms["form"]["ans"].value;
+
+  selected == "yes" ? info[idx+1] = 1 : info[idx+1] = 0;
+
+
+
+
+  document.getElementById("form").reset();
+
+  idx++;
+  if (idx == questions.length-1) {
     button.innerHTML= "Done";
+  } else if (idx == questions.length) {
+    button.style.display = "none";
+    document.getElementById('question').innerHTML = "Thank you!";
+  }
+  if (idx < questions.length) {
+    document.getElementById("title").innerHTML = questions[idx];
   }
 
 });
